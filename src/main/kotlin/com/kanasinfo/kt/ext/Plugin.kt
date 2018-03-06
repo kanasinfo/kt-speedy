8ece0b7fcedec6aadb275a9db95994c8ee00cf13
gefangshuai <gefangshuai@163.com>
2018-03-06 18:15:07 +0800

    coding

---
+++ src/main/kotlin/com/kanasinfo/kt/ext/Plugin.kt
@@ -0,0 +1,8 @@
+package com.kanasinfo.kt.ext
+
+@Target(allowedTargets = [(AnnotationTarget.CLASS)])
+annotation class NoArg
+
+
+@Target(allowedTargets = [(AnnotationTarget.CLASS)])
+annotation class AllOpen
