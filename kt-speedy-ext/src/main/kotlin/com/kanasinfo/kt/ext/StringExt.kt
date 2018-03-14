d150fec54602fb5fa56b67aa596c85a0a5978023
gefangshuai <gefangshuai@163.com>
2018-03-14 09:59:55 +0800

    fix bugs

--- kt-speedy-ext/src/main/kotlin/com/kanasinfo/kt/ext/StringExt.kt
+++ kt-speedy-ext/src/main/kotlin/com/kanasinfo/kt/ext/StringExt.kt
@@ -15,8 +15,18 @@ fun String.getFileType(): String {
 }
 
 /**
- * like查询拼接
+ * like 查询拼接
  */
 fun String.toLikeQuery(): String {
     return "%$this%"
 }
+
+/**
+ * like 左侧模糊匹配
+ */
+fun String.toLeftLikeQuery() = "%$this"
+
+/**
+ * like 右侧模糊匹配
+ */
+fun String.toRightLikeQuery() = "$this%"
