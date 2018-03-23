5c56a710cf2c0b2287055acb88b5a130cffdacc3
gefangshuai <gefangshuai@163.com>
2018-03-23 17:25:11 +0800

    fix bugs

--- kt-speedy-ext/src/main/kotlin/com/kanasinfo/kt/ext/StringExt.kt
+++ kt-speedy-ext/src/main/kotlin/com/kanasinfo/kt/ext/StringExt.kt
@@ -1,5 +1,9 @@
 package com.kanasinfo.kt.ext
 
+import java.util.Random
+
+
+
 /**
  * 路径追加
  */
@@ -47,4 +51,26 @@ fun String.substringMax(count: Int): String {
  */
 fun String?.isPresent(): Boolean {
     return !this.isNullOrBlank()
+}
+
+class StringExt {
+    companion object {
+        /**
+         * 生成指定长度的随机字符串
+         */
+        fun randomStr(len: Int): String {
+            var i: Int  //生成的随机数
+            var count = 0 //生成的密码的长度
+            // 密码字典
+            val str = charArrayOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+            val stringBuffer = StringBuffer("")
+            val r = Random()
+            while (count < len) {
+                i = r.nextInt(str.size)
+                stringBuffer.append(str[i])
+                count++
+            }
+            return stringBuffer.toString()
+        }
+    }
 }
