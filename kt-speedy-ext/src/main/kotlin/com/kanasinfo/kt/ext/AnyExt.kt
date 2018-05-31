b594aa588e6efbe75a887089fb7f68d98283a647
gefangshuai <gefangshuai@163.com>
2018-05-31 20:31:01 +0800

    coding

--- kt-speedy-ext/src/main/kotlin/com/kanasinfo/kt/ext/AnyExt.kt
+++ kt-speedy-ext/src/main/kotlin/com/kanasinfo/kt/ext/AnyExt.kt
@@ -1,12 +1,16 @@
 package com.kanasinfo.kt.ext
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 
 fun Any.toJson(): String{
-    return ObjectMapper().writeValueAsString(this)
+    return jacksonObjectMapper().writeValueAsString(this)
 }
 fun Any?.notNull(f: ()-> Unit){
     if (this != null){
         f()
     }
+}
+fun Any.toMap(): Map<*, *>? {
+    return jacksonObjectMapper().readValue(this.toJson())
 }
