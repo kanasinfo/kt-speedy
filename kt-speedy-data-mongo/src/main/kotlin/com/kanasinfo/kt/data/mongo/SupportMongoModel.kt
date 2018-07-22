32ca4a58ec2773eb97b5b106817a46065e165072
gefangshuai <gefangshuai@163.com>
2018-07-22 13:38:08 +0800

    fix bugs

---
+++ kt-speedy-data-mongo/src/main/kotlin/com/kanasinfo/kt/data/mongo/SupportMongoModel.kt
@@ -0,0 +1,35 @@
+package com.kanasinfo.kt.data.mongo
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+import javax.persistence.EntityListeners
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+import java.util.Date
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+class SupportMongoModel {
+    @Id
+    @JsonSerialize(using = ToStringSerializer::class)
+    var id: ObjectId? = null
+
+    @CreatedDate
+    var createdDate: Date? = null
+
+    @LastModifiedDate
+    var lastModifiedDate: Date? = null
+
+    @CreatedBy
+    var createdBy: ObjectId? = null
+
+    @LastModifiedBy
+    var lastModifiedBy: ObjectId? = null
+}
