1c2de155aea030796b4b9dd8e99acc7afdcace19
gefangshuai <gefangshuai@163.com>
2019-04-12 18:52:51 +0800

    fix bugs

--- kt-speedy-platform/src/main/kotlin/com/kanasinfo/platform/core/mvc/GMTTimeZoneSetListener.kt
+++ kt-speedy-platform/src/main/kotlin/com/kanasinfo/platform/core/mvc/GMTTimeZoneSetListener.kt
@@ -1,5 +1,6 @@
 package com.kanasinfo.platform.core.mvc
 
+import com.kanasinfo.ext.isPresent
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import java.util.*
@@ -15,10 +16,17 @@ class GMTTimeZoneSetListener : ServletContextListener {
     @Value("\${ks.platform.timezone.utc}")
     private var utcEnable: Boolean? = false
 
+    @Value("\${ks.platform.timezone}")
+    private var timezone: String? = null
+
     override fun contextInitialized(sce: ServletContextEvent) {
         if (utcEnable == true) {
             logger.info("set defalut timezone: GMT")
             TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+        }
+        if (timezone.isPresent()) {
+            logger.info("set defalut timezone: $timezone")
+            TimeZone.setDefault(TimeZone.getTimeZone(timezone))
         }
     }
 
