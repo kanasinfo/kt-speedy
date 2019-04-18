360474b9fe164cdd51c5ae57e7cb5639ad6190dc
jmars <ji_ma@163.com>
2019-04-18 18:13:29 +0800

    update

--- kt-speedy-ext/src/main/kotlin/com/kanasinfo/ext/AnyExt.kt
+++ kt-speedy-ext/src/main/kotlin/com/kanasinfo/ext/AnyExt.kt
@@ -3,6 +3,7 @@ package com.kanasinfo.ext
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import java.lang.IllegalArgumentException
 
 fun Any.toJson(ignoreNull: Boolean? = null): String {
     return if (true == ignoreNull) {
@@ -23,7 +24,17 @@ fun Any.toMap(): Map<String, Any> {
 }
 
 fun Any?.toMapOrNull(): Map<String, Any>? {
-    if(this == null)
+    if (this == null)
         return null
     return jacksonObjectMapper().readValue(this.toJson())
+}
+
+fun <T> required(any: T?, errorMsg: String): T {
+    return any ?: throw IllegalArgumentException(errorMsg)
+}
+
+fun <T1, T2> ifNotNull(value1: T1?, value2: T2?, bothNotNull: (T1, T2) -> (Unit)) {
+    if (value1 != null && value2 != null) {
+        bothNotNull(value1, value2)
+    }
 }
